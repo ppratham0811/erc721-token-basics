85ee1463302c5a15e478a8c713557202740f0d12
ppratham0811 <ppratham0811@gmail.com>
2022-07-01 20:50:28 +0530

    scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const nftContract = await ethers.getContractFactory("ThunderItem");
+
+    const deployedNFTContract = await nftContract.deploy();
+    console.log("NFT Contract Address: ", deployedNFTContract.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
